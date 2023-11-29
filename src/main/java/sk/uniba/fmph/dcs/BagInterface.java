490082b71d6d5883a610bc38d46cc1aafa650e4e
TimoSladek <timo6.sladek@gmail.com>
2023-11-29 13:33:08 +0100

    refactoring Game and adding BagInterface

---
+++ src/main/java/sk/uniba/fmph/dcs/BagInterface.java
@@ -0,0 +1,9 @@
+package sk.uniba.fmph.dcs;
+
+import java.util.ArrayList;
+
+public interface BagInterface {
+    ArrayList<Tile> take(int count);
+
+    String state();
+}
--- src/main/java/sk/uniba/fmph/dcs/Game.java
+++ src/main/java/sk/uniba/fmph/dcs/Game.java
@@ -6,17 +6,17 @@ public class Game implements GameInterface {
 
     private final GameObserver gameObserver;
     private final TableArea tableArea;
-    private Bag bag;
     private final ArrayList<Board> boards;
     private int currentPlayerId;
     private int startingPlayerId;
+    private boolean gameFinished;
 
-    public Game(GameObserver gameObserver, TableArea tableArea, Bag bag, ArrayList<Board> boards) {
+    public Game(GameObserver gameObserver, TableArea tableArea, ArrayList<Board> boards) {
         this.gameObserver = gameObserver;
         this.tableArea = tableArea;
-        this.bag = bag;
         this.boards = boards;
         this.currentPlayerId = 0;
+        this.gameFinished = false;
     }
 
     @Override
@@ -24,25 +24,24 @@ public class Game implements GameInterface {
         if (playerId != currentPlayerId) {
             return false;
         }
+        if (gameFinished) {
+            gameObserver.notifyEverybody("Game is finished.");
+            return false;
+        }
         ArrayList<Tile> tiles = tableArea.take(sourceId, idx);
-        if(tiles.contains(Tile.STARTING_PLAYER)){
+        if (tiles.contains(Tile.STARTING_PLAYER)) {
             startingPlayerId = currentPlayerId;
         }
         boards.get(playerId).put(destinationIdx, tiles);
-        if (tableArea.isRoundEnd()){
+        if (tableArea.isRoundEnd()) {
             currentPlayerId = startingPlayerId;
-            for (Board board: boards){
+            for (Board board : boards) {
                 FinishRoundResult result = board.finishRound();
-                if (result == FinishRoundResult.GAME_FINISHED){
-                    int mostPoints = 0, winner = 0;
-                    for (int i = 0; i < boards.size(); i++) {
-                        boards.get(i).endGame();
-                        if (boards.get(i).points.getValue() > mostPoints){
-                            mostPoints = boards.get(i).points.getValue();
-                            winner = i;
-                        }
-                    }
-                    gameObserver.notifyEverybody("GAME_FINISHED AND THE WINNER IS PLAYER NUMBER: " + winner);
+                if (result == FinishRoundResult.GAME_FINISHED) {
+                    gameFinished = true;
+                    int winner = findWinner(boards);
+                    gameObserver.notifyEverybody(
+                            "GAME_FINISHED AND THE WINNER IS PLAYER NUMBER: " + winner);
                     return true;
                 }
             }
@@ -52,5 +51,21 @@ public class Game implements GameInterface {
             gameObserver.notifyEverybody(boards.get(playerId).state());
         }
         return true;
+    }
+
+    public int getCurrentPlayerId() {
+        return currentPlayerId;
+    }
+
+    private Integer findWinner(ArrayList<Board> boards) {
+        int mostPoints = 0, winner = 0;
+        for (int i = 0; i < boards.size(); i++) {
+            boards.get(i).endGame();
+            if (boards.get(i).points.getValue() > mostPoints) {
+                mostPoints = boards.get(i).points.getValue();
+                winner = i;
+            }
+        }
+        return winner;
     }
 }
